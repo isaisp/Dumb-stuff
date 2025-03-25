0e6741aa99dcbfa64ddd7a2a98bca08688a87652
isaisp <110698701+isaisp@users.noreply.github.com>
2025-03-25 12:56:29 -0500

    Update The wrong calculator.py

--- The wrong calculator.py
+++ The wrong calculator.py
@@ -1,1 +1,16 @@
+import random
 
+subtractNumber = random.randrange(5)
+
+print("__________________________") #25
+print("|  The Wrong Calculator  |") 
+print("|________________________|")
+
+print("(This is a WIP - work in progress. so there is only addition working)")
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+
+product = number1 + number2
+answer = product - subtractNumber
+
+print(answer)
